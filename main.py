e82fe54d47c23bf368e22dfe0ce47e187c5218d4
aozodbek2197 <aozodbek2197@gmail.com>
2026-04-18 12:35:58 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+# 1-mashq
+nums = list(map(int, input().split()))
+print([i for i in nums if i % 2 == 0])
+# 2-mashq
+nums = list(map(int, input().split()))
+print([i for i in nums if i % 2 != 0])
+# 3-mashq
+nums = list(map(int, input().split()))
+print([i**2 for i in nums])
+# 4-mashq
+d = {"a": 1, "b": 2}
+print(d)
+# 5-mashq
+d = {"a": 10}
+print(d["a"])
